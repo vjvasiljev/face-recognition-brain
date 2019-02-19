3fd851619911c08f0a393791f3be77a1024fd755
VJVasiljev <v.j.vasiljev@gmail.com>
2019-02-19 23:57:23 +0200

    Add particle.js background

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Particles from "react-particles-js";
 import "./App.css";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
+// backgroundImage: `url(${logo})`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
