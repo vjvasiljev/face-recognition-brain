a1cbb73f7fc3cc82c6dfe5788a9db7c4d92a3671
VJVasiljev <v.j.vasiljev@gmail.com>
2019-02-19 22:43:11 +0200

    Add background gradient and change nav colors

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navigation = () => {
   return (
     <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-      <p className="f3 link dim black underline pa3 pointer">Sign out</p>
+      <p className="f3 link dim white underline pa3 pointer cap">SIGN OUT</p>
     </nav>
   );
 };
