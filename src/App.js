bfc6f45f31b45a666fe436d9946d22530c955b97
VJVasiljev <v.j.vasiljev@gmail.com>
2019-02-19 23:39:41 +0200

    Add Rank.js and ImageLinkForm.js components, change font

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import "./App.css";
 
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition /> */}
       </div>
