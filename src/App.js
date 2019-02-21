00e78ce3e9466d3cbcb8ee04019ed5a43423b253
VJVasiljev <v.j.vasiljev@gmail.com>
2019-02-21 21:19:04 +0200

    create state, add event listener for input

--- src/App.js
+++ src/App.js
@@ -21,6 +21,17 @@ const particlesOptions = {
 // backgroundImage: `url(${logo})`
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+
+  onInputChange = event => {
+    console.log(event);
+  };
+
   render() {
     return (
       <div className="App">
@@ -28,7 +39,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} />
         {/* <FaceRecognition /> */}
       </div>
     );
